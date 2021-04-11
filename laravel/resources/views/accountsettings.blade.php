4573ba45e80900e8df98df8eff55be95d728ee9b
Dante Mogrim <1991.mogrim@gmail.com>
2021-04-11 12:07:00 +0200

    Better forms.

--- laravel/resources/views/accountsettings.blade.php
+++ laravel/resources/views/accountsettings.blade.php
@@ -25,100 +25,103 @@
     </div>
     </div>
 
-
 <h1 class="mx-6 mr-3 my-3 text-3xl pb-3">Account settings</h1>
 @include('errors')
 
 <!-- Edit user settings. -->
 <h2 class="mx-6 mr-3 my-3 text-2xl underline">Edit user settings:</h2>
-<form class="pt-6 ml-6 bg-white shadow rounded col-span-2 max-w-screen-sm" action="edit-user" method="post">
+
+<div class="bg-white grid justify-items-center m-7 w-3/4  p-3 shadow-xl rounded-xl">
+<form class="flex flex-col" action="edit-user" method="post">
         @csrf
         <div>
-            <label class="m-5 mr-1" for="name">Name:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="name" id="name" type="name" value="{{ auth()->user()->name }}" placeholder="Person Personson" />
+            <label class="flex m-2 font-bold" for="name">Name:</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="name" id="name" type="name" value="{{ auth()->user()->name }}" placeholder="Person Personson" />
         </div>
        <div>
-            <label class="m-5 mr-1" for="email">E-mail:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="email" id="email" type="email" value="{{ auth()->user()->email }}"/>
+            <label class="flex m-2 font-bold" for="email">E-mail:</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="email" id="email" type="email" value="{{ auth()->user()->email }}"/>
         </div>
          <div>
-            <label class="m-5 mr-1" for="password">Password:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="password" id="password" type="password" value="{{ auth()->user()->password }}" />
+            <label class="flex m-2 font-bold" for="password">Password:</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="password" id="password" type="password" value="{{ auth()->user()->password }}" />
         </div>
 
         <div>
-            <label class="m-5 mr-1" for="phone">Phone:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="phone" id="phone" type="text" placeholder="070-1234567"/>
+            <label class="flex m-2 font-bold" for="phone">Phone (+46):</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="phone" id="phone" type="text" value="{{ auth()->user()->phone }}"/>
         </div>
 
         <div>
-            <label class="m-5 mr-1" for="info">Info:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="info" id="info" type="text" placeholder="Write something about yourself."/>
-        </div>
-
-        <div>
-            <label class="m-5 mr-1" for="neighborhood">Neighborhood:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="neighborhood" id="neighborhood" type="text" placeholder="Which neighborhood do you live in?"/>
+            <label class="flex m-2 font-bold" for="info">Info:</label>
+            <textarea class="flex m-2 w-full border-4 border-green-400 px-3 py-2" rows="5" cols="60" name="info" id="info" placeholder="{{ auth()->user()->info }}"></textarea>
         </div>
         <div>
-            <label class="m-5 mr-1" for="pet-owner">Do you own a pet?:</label>
-            <select name="pet-owner" id="pet-owner">
-                <option class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-owner" id="pet-owner" value="Yes">Yes</option>
-                <option class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-owner" id="pet-owner" value="No">No</option>
+            <label class="flex m-2 font-bold" for="neighborhood">Neighborhood:</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="neighborhood" id="neighborhood" type="text" value="{{ auth()->user()->neighborhood }}"/>
+        </div>
+        <div>
+            <label class="flex m-2 font-bold" for="pet-owner">Do you own a pet?:</label>
+            <select class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="pet-owner" id="pet-owner">
+                <option class="" name="pet-owner" id="pet-owner" value="Yes">Yes</option>
+                <option class="" name="pet-owner" id="pet-owner" value="No">No</option>
             </select>
-
         </div>
         <div>
-            <label class="m-5 mr-1" for="pet-walker">Do you want to walk with a pet?:</label>
-            <select name="pet-walker" id="pet-walker">
-                <option class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-walker" id="pet-walker" value="Yes">Yes</option>
-                <option class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-walker" id="pet-walker" value="No">No</option>
+            <label class="flex m-2 font-bold" for="pet-walker">Do you want to walk with a pet?:</label>
+            <select class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="pet-walker" id="pet-walker">
+                <option class="" name="pet-walker" id="pet-walker" value="Yes">Yes</option>
+                <option class="" name="pet-walker" id="pet-walker" value="No">No</option>
             </select>
-
         </div>
         <button class="m-5 mr-4 py-2 px-4 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" type="submit">Update</button>
 </form>
+</div>
 
 <hr class="w-auto h-2 mt-10 bg-blue-200">
 <br>
 
-
 <!-- Edit pet settings. -->
 <h2 class="mx-6 mr-3 my-3 text-2xl underline">Add a pet:</h2>
-<form class="pt-6 ml-6 bg-white shadow rounded col-span-2 max-w-screen-sm" action="add-pet" method="post">
+<div class="bg-white m-7 w-3/4  p-3 shadow-xl rounded-xl">
+<form class="p-3 flex flex-col" action="add-pet" method="post">
         @csrf
         <div>
-            <label class="m-5 mr-1" for="name">Name:</label>
-            <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-name" id="pet-name" type="pet-name" placeholder="Doggo" />
+            <label class="flex m-2 font-bold" for="name">Name:</label>
+            <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="pet-name" id="pet-name" type="pet-name" placeholder="Doggo" />
         </div>
         <div>
 
-                <label class="m-5 mr-1" for="info">Info:</label>
-                <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-info" id="pet-info" type="pet-info" placeholder="Loves fetch."/>
+                <label class="flex m-2 font-bold" for="info">Info:</label>
+                <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="pet-info" id="pet-info" type="pet-info" placeholder="Loves fetch."/>
             </div>
             <div>
 
-                    <label class="m-5 mr-1" for="species">Species:</label>
-                    <input class="my-2 bg-white-100 border-green-500 border-2 rounded-sm" name="pet-species" id="pet-species" type="pet-species" placeholder="Dog, Shiba Inu." />
+                    <label class="flex m-2 font-bold" for="species">Species:</label>
+                    <input class="flex m-2 w-full border-4 border-green-400 px-3 py-2" name="pet-species" id="pet-species" type="pet-species" placeholder="Dog, Shiba Inu." />
                 </div>
             <button class="m-5 mr-4 py-2 px-4 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" type="submit">Update</button>
     </form>
-<br>
+</div>
 
 <hr class="w-auto h-2 bg-blue-200">
 <br>
 
 
+
 <!-- Delete account -->
 <h2 class="mx-6 mr-3 my-3 text-2xl underline">Delete account:</h2>
-<form class="pt-6 ml-6 bg-red-100 shadow rounded col-span-2 max-w-screen-sm" action="delete-user" method="post">
+<div class="pt-5 bg-red-200 m-7 w-3/4 shadow-xl rounded-xl">
+<form class="flex flex-col" action="delete-user" method="post">
         @csrf
-        <div>
-            <label class="m-5 mr-1" for="name">Are you sure you want to delete your account and all associated data?</label>
-            <input class="my-2 bg-white-100  border-red-500 border-2 rounded-sm" name="delete-check" id="delete-check" type="checkbox" required />
+        <div class="p-3">
+            <label class="m-2 font-bold" for="name">Are you sure you want to delete your account and all associated data?</label>
+            <input class="" name="delete-check" id="delete-check" type="checkbox" required />
         </div>
             <button class="m-5 mr-4 py-2 px-4 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" type="submit" id="delete-user">Delete</button>
     </form>
+</div>
+
 <br>
 
 <a href="/dashboard"><button class="mb-5 ml-6 py-2 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" type="submit">Back</button></a>
