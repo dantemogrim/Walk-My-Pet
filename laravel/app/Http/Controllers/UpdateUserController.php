f5fa3ccb07ed85600eb50e85c2eca38b33390e8b
Erik <nausea87@users.noreply.github.com>
2021-04-14 07:55:59 +0000

    Apply fixes from StyleCI

--- laravel/app/Http/Controllers/UpdateUserController.php
+++ laravel/app/Http/Controllers/UpdateUserController.php
@@ -13,7 +13,6 @@ class UpdateUserController extends Controller
 {
     public function __invoke(Request $request, User $user)
     {
-
         $user = Auth::user();
         $user->name = $request->input('name');
         $user->email = $request->input('email');
